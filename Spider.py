f09e768d7ec011891f5372acf4d6d355d291bd41
Maru-zhang <maru-zhang@foxmail.com>
2016-05-23 14:21:07 +0800

    改成了MySQLDB；

--- Spider.py
+++ Spider.py
@@ -10,95 +10,96 @@ from BeautifulSoup import  BeautifulSoup
 
 class ParserPage():
 
+    video_url = 'http://v8.ccut.edu.cn/article.php?/'
+    news_url = 'http://news.ccut.edu.cn/sort.php/'
+
     def __init__(self):
-        last_v = Video.query.last()
-        last_n = News.query.last()
+        last_v = Video.query.order_by(Video.id.desc()).first()
+        last_n = News.query.order_by(News.id.desc()).first()
 
         if last_v == None:
             self.v_index = 0
         else:
-            self.v_index = last_v.id
+            self.v_index = int(last_v.id)
+
         if last_n == None:
             self.n_index = 0
         else:
-            self.n_index = last_n.id
-        self.video_url = 'http://v8.ccut.edu.cn/article.php?/'
-        self.news_url = 'http://news.ccut.edu.cn/sort.php/'
-
-    def parservideo(self,index):
+            self.n_index = int(last_n.id)
 
 
-        print self.video_url + str(index)
-        try:
-            html = urllib2.urlopen(self.video_url + str(index)).read().decode("gb2312")
-        except UnicodeDecodeError,e:
-            print("UnicodeDecodeError:"+e)
-            return
-        except urllib2.URLError,e:
-            print("URLError"+e)
-            return
 
-        patName = re.compile(r'<font class=bigfont><b>(.*)</b>')
-        patURL = re.compile(r'href="(.*\.rmvb|.*\.mp4)"')
-        patList = re.compile(r'<a href="http://v8\.ccut\.edu\.cn/sort\.php\?/\d*">(.*?)</a>')
+    def parserVideo(self):
+
+        url = 'http://v8.ccut.edu.cn/article.php?/%s' % self.v_index
+        print url
 
         try:
+            html = urllib2.urlopen(url).read().decode("gb2312")
+
+            patName = re.compile(r'<font class=bigfont><b>(.*)</b>')
+            patURL = re.compile(r'href="(.*\.rmvb|.*\.mp4)"')
+            patList = re.compile(r'<a href="http://v8\.ccut\.edu\.cn/sort\.php\?/\d*">(.*?)</a>')
+
             resName = re.findall(patName,html)[0]
             resURL = re.findall(patURL,html)[0]
             resList = re.findall(patList,html)
 
-        except IndexError,e:
+            video = Video(id=self.v_index,title=resName, url=resURL, leve1=resList[0], leve2=resList[1])
+            db.session.add(video)
+            db.session.commit()
+
+            print("Success save to database --- %s" % resName)
+        except Exception,e:
+            print("Error:%s" % e)
+            db.session.rollback()
+            self.v_index += 1
+
             return
 
-        video = Video(id=index,title=resName, url=resURL, leve1=resList[0], leve2=resList[1])
-        db.session.add(video)
-        db.session.commit()
-        print("Success save to database --- %s" % resName)
 
-
-    def parserNews(self,index):
+    def parserNews(self):
 
         try:
-            html = urllib2.urlopen(self.news_url + str(index)).read().decode("gb2312")
-        except UnicodeDecodeError,e:
-            print("UnicodeDecodeError:"+e)
-            return
-        except urllib2.URLError,e:
-            print("URLError"+e)
-            return
-
-        try:
+            html = urllib2.urlopen(self.news_url + str(self.n_index)).read().decode("gb2312")
             soup = BeautifulSoup(html, fromEncoding="gb2312")
             for content in soup.findAll(colspan='2'):
                 title = content.find(target='_blank').string
                 newURL = content.a["href"]
                 time = content.find(color='#666666').string
-                new = News(id=index,title=title,url=newURL,datetime=time)
+                new = News(id=self.n_index,title=title,url=newURL,datetime=time)
                 db.session.add(new)
                 db.session.commit()
+                self.n_index += 1
                 print('Success save to database. ---- %s' % title)
-        except IndentationError,e:
-            print(e)
-        except AttributeError,e:
-            print(e)
+        except Exception,e:
+            print("Error:"+e)
+            self.n_index += 1
+            db.session.rollback()
+            return
 
 
 if __name__ == "__main__":
 
-    index = 1
     parser = ParserPage()
 
-    while index < 2000:
-        try:
-            result = parser.parserVideo(index=index)
-        except TypeError,e:
-            print("TypeError-----%s" % index)
-            continue
-        except Exception,e:
-            print("resultError-----%s" % index)
-            continue
-        finally:
-            index += 1
+    count = 1
+
+    while count < 21457:
+        parser.parserVideo()
+        count+=1
+        # try:
+        #     parser.parserVideo()
+        # except TypeError,e:
+        #     print("TypeError-----%s",e)
+        #     continue
+        # except Exception,e:
+        #     print("resultError-----%s",e)
+        #     continue
+        # finally:
+        #     count+=1
+
+
 
 
     # while index < 50:
--- config.py
+++ config.py
@@ -2,7 +2,14 @@ __author__ = 'Maru'
 
 import os
 
+dbhost = 'localhost:3388'
+dbuser = 'root'
+dbpass = ''
+dbname = 'iCCUT'
+DB_URI = 'mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
